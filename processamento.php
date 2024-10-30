8e8ebec9d77dc6ec6c7f62f40e403e8db8c528d5
Kawdevlops <kauany.violin@gmail.com>
2024-10-30 16:38:07 -0300

    30/10/2024
    Criação de interação no formulario com PHP

---
+++ processamento.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Processamento </title>
+</head>
+<body>
+    <h1>Processamento dos dados vindo do formulário</h1>
+    <hr>
+
+
+<?php // Capturando os dados através do array $_POST e guardando cada um em uma variavel
+//dump mostra o array estruturado
+    //var_dump
+    $nome = $_POST ["nome"];
+    $email = $_POST ["email"];
+    $nascimento = $_POST ["nascimento"];
+    $mensagem = $_POST ["mensagem"];
+?>
+
+
+    <h2> Dados Recebidos </h2>
+    <ul>
+        <li> Nome: <?= $nome?></li>
+        <li> E-mail: <?= $email?></li>
+        <li> Data de nascimento: <?= $nascimento?></li>
+        <li> Mensagem: <?= $mensagem?></li>
+        
+    </ul>
+    
+</body>
+</html>
